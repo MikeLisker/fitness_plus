a593ddb617bdd10e696f1457fb9418a3313c0b99
Michael Lisker <est.michael.lisker@unimilitar.edu.co>
2024-10-19 22:33:11 -0500

    main-java-json

--- script.js
+++ script.js
@@ -1,15 +1,52 @@
-// Selecciona los elementos por su ID
-const showOverlayButton = document.getElementById("showOverlay");
-const overlayElement = document.getElementById("overlayElement");
-const closeOverlayButton = document.getElementById("closeOverlay");
+// Ejecutar todo el código solo cuando el DOM esté completamente cargado
+document.addEventListener('DOMContentLoaded', () => {
+  // 1. Volver a la página anterior
+  const backButton = document.querySelector('#backButton');
+  if (backButton) {
+      backButton.addEventListener('click', () => {
+          history.back();
+      });
+  }
 
-// Mostrar el overlay al hacer clic en el botón "Mostrar Mensaje"
-// Asigna un evento 'click' a cada botón que muestra el overlay
-showOverlayButton.addEventListener("click", () => {
-    overlayElement.style.display = "flex"; // Muestra el overlay
+  // 2. Cambiar estado de botones (solo un botón activo a la vez)
+  const stateButtons = document.querySelectorAll('.fecha-card');
+  stateButtons.forEach(button => {
+      button.addEventListener('click', () => {
+          stateButtons.forEach(btn => btn.classList.remove('active'));
+          button.classList.add('active');
+      });
   });
 
-// Ocultar el overlay al hacer clic en el botón "Cerrar"
-closeOverlayButton.addEventListener("click", () => {
-  overlayElement.style.display = "none"; // Oculta el overlay
+  // 3. Mostrar mensaje sobrepuesto al hacer clic en un botón y ocultarlo después de 3 segundos
+  // Seleccionar todos los botones de reserva
+  const reserveButtons = document.querySelectorAll('.showOverlay');
+  // Seleccionar todos los mensajes sobrepuestos
+  const overlayMessages = document.getElementById('overlayElement');
+  // Seleccionar los botones de cerrar
+  const closeButtons = document.getElementById('closeOverlay');
+
+  const sectionHide = document.getElementById('section-hide');
+
+  const showSection = document.getElementById('show-section');
+  
+  showSection.addEventListener('click', () => {  
+    sectionHide.style.display = "flex";
+    });
+  
+  // Añadir eventos a los botones de reserva
+  reserveButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      overlayMessages.style.display = "flex"; // Mostrar mensaje
+    });
+  });
+
+  closeButtons.addEventListener('click', () => {
+    overlayMessages.style.display = 'none';
+    sectionHide.style.display = "none";
+  });
+
+  
+
+
+  
 });
